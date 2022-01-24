09c45ddf4a89fb7a97017490534d16cef0bf2d7c
Kami Kaze <joscha.vack@vi-solutions.de>
2022-01-24 19:57:14 +0100

    Pull skin install and add command line version

--- MainWindow.cs
+++ MainWindow.cs
@@ -98,121 +98,23 @@ namespace Titanfall2_SkinTool
 
         private void Test_Click(object sender, EventArgs e)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            String msg;
             try
             {
-                if (File.Exists(GamePath + "\\r5apex.exe") || File.Exists(GamePath + "\\Titanfall2.exe"))
-                {
-                    //fixed
-                }
-                else
-                {
-                    throw new MyException(rm.GetString("GameLoadFailed"));
-                }
-                if (!File.Exists(PathText.Text) || PathText.Text == "")
-                {
-                    throw new MyException(rm.GetString("ZipLoadFailed"));
-                }
+                SkinTool tool = new SkinTool(GamePath, SelectedGame, (m) => textBox1.AppendText(m + "\r\n"));
+                tool.InstallSkin(PathText.Text);
 
-                string ExtractPath = filePath + "\\" + rm.GetString("SaveFolder") + "\\" + Folder;
-                if (!Directory.Exists(ExtractPath))
-                {
-                    Directory.CreateDirectory(ExtractPath);
-                }
-                try
-                {
-                    ZipFile.ExtractToDirectory(PathText.Text, ExtractPath, Encoding.GetEncoding("GBK"));
-                    textBox1.AppendText(rm.GetString("ZipLoadSuccess") + "\r\n");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-                //Testing new way to save dds
-                List<string> FileList = new List<string>();
-                FindSkinFiles(ExtractPath, FileList, ".dds");
-
-                foreach (var i in FileList)
-                    Console.WriteLine(i);
-
-                DDSFolderExist = FileList.Count;
-                if (DDSFolderExist == 0)
-                {
-                    throw new MyException(rm.GetString("FindSkinFailed"));
-                }
-
-                foreach (var i in FileList)
-                {
-                    int FolderLength = ExtractPath.Length;
-                    String FileString = i.Substring(FolderLength);
-                    int imagecheck = ImageCheck(i);
-                    //the following code is waiting for the custom model
-                    Int64 toseek = 0;
-                    int tolength = 0;
-                    int totype = 0;
-                    //传递数组内容
-                    //需要使用命名对代码进行优化
-                    if (IsPilot(i))
-                    {
-                        Titanfall2.PilotData.PilotDataControl pdc = new Titanfall2.PilotData.PilotDataControl(i, imagecheck);
-                        toseek = Convert.ToInt64(pdc.Seek);
-                        tolength = Convert.ToInt32(pdc.Length);
-                        totype = Convert.ToInt32(pdc.SeekLength);
-                    }
-                    else //if(IsWeapon(i))
-                    {
-                        if (SelectedGame == "APEX")
-                        {
-                            APEX.WeaponData.WeaponDataControl wdc = new APEX.WeaponData.WeaponDataControl(i, imagecheck);
-                            toseek = Convert.ToInt64(wdc.FilePath[0, 1]);
-                            tolength = Convert.ToInt32(wdc.FilePath[0, 2]);
-                            totype = Convert.ToInt32(wdc.FilePath[0, 3]);
-                        }
-                        else if (SelectedGame == "Titanfall2")
-                        {
-                            Titanfall2.WeaponData.WeaponDataControl wdc = new Titanfall2.WeaponData.WeaponDataControl(i, imagecheck);
-                            toseek = Convert.ToInt64(wdc.FilePath[0, 1]);
-                            tolength = Convert.ToInt32(wdc.FilePath[0, 2]);
-                            totype = Convert.ToInt32(wdc.FilePath[0, 3]);
-                        }
-                    }
-                    StarpakControl sc = new StarpakControl(i, toseek, tolength, totype, GamePath, SelectedGame, imagecheck, "Replace");
-                    //ToDo:Change to the Struct,still not done that...
-                }
-
-                FileList.Clear();
-                msg = rm.GetString("InstallSuccess");
-                textBox1.AppendText(msg + "\r\n");
-                MessageBox.Show(msg, rm.GetString("Tip"), MessageBoxButtons.OK);
+                // success
+                msg = "Install succes!";
             }
-            catch (MyException myException)
+            catch (Exception ex)   // Catches MyException as well
             {
-                MessageBox.Show(myException.Message);
+                // failed
+                msg = "Install failed: " + ex.Message;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            GC.Collect();
-            if (Directory.Exists(rm.GetString("SaveFolder")))
-            {
-                try
-                {
-                    DirectoryInfo tempDir = new DirectoryInfo(rm.GetString("SaveFolder"));
 
-                    foreach (DirectoryInfo dir in tempDir.EnumerateDirectories())
-                    {
-                        dir.Delete(true);
-                    }
-
-                    tempDir.Delete();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error occured while trying to delete the temporary files folder: \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
+            // info popup
+            MessageBox.Show(msg, rm.GetString("Tip"), MessageBoxButtons.OK);
         }
 
         private void Language_Default()
@@ -423,86 +325,6 @@ namespace Titanfall2_SkinTool
         {
             SkinPackMakerWindow window = new SkinPackMakerWindow(SelectedGame);
             window.ShowDialog();
-        }
-
-        private void FindSkinFiles(string FolderPath, List<string> FileList, string FileExtention)
-        {
-            try
-            {
-                DirectoryInfo di = new DirectoryInfo(FolderPath);
-                FileSystemInfo[] fi = di.GetFileSystemInfos();
-                foreach (var i in fi)
-                {
-                    if (i is DirectoryInfo)
-                    {
-                        FindSkinFiles(i.FullName, FileList, FileExtention);
-                    }
-                    else
-                    {
-                        if (i.Extension == FileExtention)
-                        {
-                            FileList.Add(i.FullName);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private int ImageCheck(String ImageName)
-        {
-            int result = -1;
-            int temp = ImageName.LastIndexOf("\\");
-            ImageName = ImageName.Substring(0, temp);
-            temp = ImageName.LastIndexOf("\\") + 1;
-            ImageName = ImageName.Substring(temp, ImageName.Length - temp);
-            switch (ImageName)
-            {
-                case "256x128":
-                case "256x256":
-                case "256":
-                    //Big change,I don't want to do it:(
-                    break;
-                case "512x256":
-                case "512x512":
-                case "512":
-                    result = 0;
-                    break;
-                case "1024x512":
-                case "1024x1024":
-                case "1024":
-                    result = 1;
-                    break;
-                case "2048x1024":
-                case "2048x2048":
-                case "2048":
-                    result = 2;
-                    break;
-                case "4096x2048":
-                case "4096x4096":
-                case "4096":
-                    result = 3;
-                    break;
-                default:
-                    result = -1;
-                    break;
-            }
-            return result;
-        }
-
-        private bool IsPilot(string Name)
-        {
-            if (Name.Contains("Stim_") || Name.Contains("PhaseShift_") || Name.Contains("HoloPilot_") || Name.Contains("PulseBlade_") || Name.Contains("Grapple_") || Name.Contains("AWall_") || Name.Contains("Cloak_") || Name.Contains("Public_"))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
 
         private void MainWindow_OnShown(object sender, EventArgs e)
--- Program.cs
+++ Program.cs
@@ -1,19 +1,104 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CommandLine;
+using System.Runtime.InteropServices;
 
 namespace Titanfall2_SkinTool
 {
+    public class Options
+    {
+        [Value(0, HelpText = "Path/To/Game/Directory", Required = true)]
+        public string GamePath { get; set; }
+
+
+        [Value(1, HelpText = "List of <path/to/skin.zip> to install.", Required = true)]
+        public IEnumerable<string> InputFiles { get; set; }
+
+        [Option('v', "verbose", Required = false, HelpText = "Enable verbose logging")]
+        public bool Verbose { get; set; }
+
+        [Option('w', "wait", Required = false, HelpText = "Wait for user input before closing console window")]
+        public bool Wait { get; set; }
+    }
+
     static class Program
     {
+        /// <summary>
+        /// Used to display console if used with commandline arguments
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
+        {
+            if (0 < args.Length)
+            {
+                // args -> is commandline
+                CommandlineMain(args);
+            }
+            else
+            {
+                // no args -> default windowed application
+                UiMain();
+            }
+        }
+
+        static void CommandlineMain(string[] args)
+        {
+            bool wait = false;
+            AllocConsole();
+
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    wait = o.Wait;
+                    Action<string> messageHandler = o.Verbose ? m => { Console.WriteLine(m); } : m => { };
+
+                    messageHandler($"GamePath={o.GamePath}");
+                    messageHandler($"InputFiles=[{string.Join(", ", o.InputFiles)}]");
+                    messageHandler($"Verbos={o.Verbose}");
+
+                    try
+                    {
+                        SkinTool tool = new SkinTool(o.GamePath, messageHandler);
+
+                        foreach (string file in o.InputFiles)
+                        {
+                            messageHandler(Path.GetFileNameWithoutExtension(file));
+
+                            tool.InstallSkin(file);
+                        }
+                    }
+                    catch (Exception ex)   // Catches MyException as well
+                    {
+                        Console.Error.WriteLine(ex.ToString());
+                        wait = true; // let user see error
+                    }
+
+                }).WithNotParsed(_ =>
+                {
+                    // argument parsing failed -> print error
+                    Console.Error.WriteLine($"Parsing options failed. See above for more information or use --help to get help.");
+                    wait = true; // let user see error
+                });
+
+            // maybe wait for user input before closing
+            if (wait)
+            {
+                Console.Write("Press any key to exit...");
+                Console.Read();
+            }
+        }
+
+        static void UiMain()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
